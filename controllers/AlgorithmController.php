ceeb19eccc45a63039e471df9ce5818b7f0ed150
bulat <bulatxajrutdinoff@yandex.ru>
2022-07-21 08:18:11 +0300

    Чистка проекта от ненужного и enablePrettyUrl
    - включил enablePrettyUrl в urlManager компоненте
    - удалил контроллер SiteController
    - удалил модели
    - создал AlgorithmController

---
+++ controllers/AlgorithmController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+
+use yii\base\Controller;
+
+class AlgorithmController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
---
+++ views/algorithm/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
+
+<?= Html::tag('h1', 'Algorithms') ?>
