abb287d10d36dc5b24e79f412a89425ca9fe7855
bulat <bulatxajrutdinoff@yandex.ru>
2022-07-22 10:12:46 +0300

    Алгоритм Евклида
    Алгоритм для нахождения наибольшего общего делителя двух целых чисел (НОД)

--- controllers/AlgorithmController.php
+++ controllers/AlgorithmController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\BinarySearch;
+use app\models\EuclideanAlgorithm;
 use app\models\SelectionSort;
 use yii\base\Controller;
 
@@ -29,6 +30,14 @@ class AlgorithmController extends Controller
         $selection_sort->setList([3,1,4,6,10,8,2]);
         echo sprintf('Array: <pre>%s</pre><br>', json_encode($selection_sort->getList(), JSON_PRETTY_PRINT));
         echo sprintf('Sorted: <pre>%s</pre><br>', json_encode($selection_sort->sort(), JSON_PRETTY_PRINT));
+        die;
+    }
+
+    public function actionEuclideanAlgorithm()
+    {
+        $euclidean_algorithm = new EuclideanAlgorithm();
+        $euclidean_algorithm->setNumbers(1071, 462);
+        echo sprintf('GCD (greatest common divisor): %s<br>', $euclidean_algorithm->getGcd());
         die;
     }
 }
